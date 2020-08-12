c4fe2445e4335b69d43e75f547746ebba65e99e1
Joseph Flatt <joseph@flattandsons.com>
2020-08-12 08:09:00 -0700

    Issue #12: Fixing the check for line length.

--- coder_sniffer/Backdrop/Sniffs/Files/LineLengthSniff.php
+++ coder_sniffer/Backdrop/Sniffs/Files/LineLengthSniff.php
@@ -43,42 +43,52 @@ class LineLengthSniff extends PHP_CodeSniffer_LineLengthSniff
      */
     public $absoluteLineLimit = 0;
 
-//
-//    /**
-//     * Checks if a line is too long.
-//     *
-//     * @param PHP_CodeSniffer_File $phpcsFile   The file being scanned.
-//     * @param int                  $stackPtr    The token at the end of the line.
-//     * @param string               $lineContent The content of the line.
-//     *
-//     * @return void
-//     */
-//    protected function checkLineLength($phpcsFile, $tokens, $stackPtr)
-//    {
-////        $backtrace = debug_backtrace();
-////        foreach ($backtrace as $key => $value) {
-////          print_r($value['file'] . "\n");
-////          print_r($value['line'] . "\n");
-////          print_r($value['function'] . "\n");
-////          print_r("\n");
-////        }
-//
-//
-//        if ($tokens[$stackPtr]['code'] === T_DOC_COMMENT || $tokens[$stackPtr]['code'] === T_COMMENT) {
-//            if (preg_match('/^[[:space:]]*(\/\*)?\*[[:space:]]*@link.*@endlink[[:space:]]*/', $lineContent) === 1) {
-//                // Allow @link documentation to exceed the 80 character limit.
-//                return;
-//            }
-//
-//            if (preg_match('/^[[:space:]]*((\/\*)?\*|\/\/)[[:space:]]*@see.*/', $lineContent) === 1) {
-//                // Allow @see documentation to exceed the 80 character limit.
-//                return;
-//            }
-//
-//            parent::checkLineLength($phpcsFile, $tokens, $stackPtr);
-//        }
-//
-//    }//end checkLineLength()
+    /**
+     * Variable to store the code from each line of the file so that the
+     * complete line can be checked with regular expressions.
+     *
+     * @var array
+     */
+    public $code = array();
+
+    /**
+     * Checks if a line is too long.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile   The file being scanned.
+     * @param int                  $stackPtr    The token at the end of the line.
+     * @param string               $lineContent The content of the line.
+     *
+     * @return void
+     */
+    protected function checkLineLength($phpcsFile, $tokens, $stackPtr)
+    {
+
+      if (empty($this->code)) {
+        foreach ($tokens as $token) {
+          if (isset($this->code[$token['line']])) {
+            $this->code[$token['line']] .= $token['content'];
+          }
+          else {
+            $this->code[$token['line']] = $token['content'];
+          }
+        }
+      }
+
+      if (isset($tokens[$stackPtr])) {
+          if (preg_match('/^[[:space:]]*(\/\*)?\*[[:space:]]*@link.*@endlink[[:space:]]*/', $this->code[$tokens[$stackPtr]['line']]) === 1) {
+              // Allow @link documentation to exceed the 80 character limit.
+              return;
+          }
+
+          if (preg_match('/^[[:space:]]*((\/\*)?\*|\/\/)[[:space:]]*@see.*/', $this->code[$tokens[$stackPtr]['line']]) === 1) {
+              // Allow @see documentation to exceed the 80 character limit.
+              return;
+          }
+
+          parent::checkLineLength($phpcsFile, $tokens, $stackPtr);
+      }
+
+    }//end checkLineLength()
 
 
     /**
